c1f7ea8298c55cb2e10dd6c0c4b5cc2db05dc541
Pieter <pieter.tenret@vitalstorm.com>
2020-12-02 09:50:32 -0600

    initial commit

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -6,6 +6,10 @@ exports.components = {
   "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-contact-form-js": () => import("./../src/pages/contact_form.js" /* webpackChunkName: "component---src-pages-contact-form-js" */),
-  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-templates-our-services-js": () => import("./../src/templates/ourServices.js" /* webpackChunkName: "component---src-templates-our-services-js" */),
+  "component---src-templates-reviews-js": () => import("./../src/templates/reviews.js" /* webpackChunkName: "component---src-templates-reviews-js" */),
+  "component---src-templates-coupons-js": () => import("./../src/templates/coupons.js" /* webpackChunkName: "component---src-templates-coupons-js" */),
+  "component---src-templates-servicepage-js": () => import("./../src/templates/servicepage.js" /* webpackChunkName: "component---src-templates-servicepage-js" */)
 }
 
