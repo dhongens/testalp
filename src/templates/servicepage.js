16806d94bba935598cd0df4b24ace73760d8c05b
Pieter <pieter.tenret@vitalstorm.com>
2020-12-15 09:42:19 -0600

    Remove page title

--- src/templates/servicepage.js
+++ src/templates/servicepage.js
@@ -166,7 +166,6 @@ export default ({ data }) => (
                                 opacity: "0.7"
                         }}>
                         </div>
-                            <h1>{data.sanityPages.pagetitle} Services</h1>
                             <hr style={{ backgroundColor:  data.sanityCompanyInfo.accentcolor.hex }} />
 
                     </div>
