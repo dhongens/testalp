d8dc840f4949b1ba85055f64795d8788d71d1fbe
Pieter <pieter.tenret@vitalstorm.com>
2022-04-20 11:34:41 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-sanity',
       options: {
-        projectId: '7zqpv0yu',
+        projectId: 'x8rpks50',
         dataset: 'production', 
         overlayDrafts: true,
         watchMode: true,
